b8b8efd704c46c1210492794ff7602ca486a2d13
Aldiyar Batyrbekov <aldibatyrbekov@gmail.com>
2019-04-29 12:16:19 -0700

    added tbeyond function

--- app.js
+++ app.js
@@ -3,3 +3,18 @@ function jediName(firstName, lastName) {
   return lastName.substring(0, 3)+firstName.substring(0,2);
 }
 console.log(jediName('Zack', 'Johnson'));
+
+
+function beyond(num) {
+  if (!isFinite(num)) {
+    return 'And beyond';
+  } else if (isFinite(num) && num > 0) {
+    return 'To Infinity';
+  } else if (isFinite(num) && num < 0) {
+    return 'To negative Infinity';
+  } else {
+    return 'Staying home'
+  }
+}
+
+console.log(beyond(-14));
